acf67eaa9702d23aa455ac86db152339579ea8cf
shrub <shrub719@gmail.com>
2026-02-19 13:21:51 +0000

    use hold note end instead of hold note duration

--- src/maps/test.rs
+++ src/maps/test.rs
@@ -33,22 +33,22 @@ pub fn test() -> Map {
     Map {
         notes: [
             note!(tap, 1000, 1.0),
-            note!(hold, 1000, 0.0, 800),
+            note!(hold, 1000, 0.0, 1800),
             note!(tap, 1250, 0.5),
             note!(tap, 1500, 0.5),
             note!(tap, 1750, 1.0),
             note!(tap, 2000, 0.0),
-            note!(hold, 2000, 1.0, 800),
+            note!(hold, 2000, 1.0, 2800),
             note!(tap, 2500, 0.6),
             note!(tap, 2625, 0.4),
             note!(tap, 2750, 0.2),
             note!(tap, 2875, 0.0),
             note!(tap, 3000, 1.0),
-            note!(hold, 3000, 0.0, 400),
+            note!(hold, 3000, 0.0, 3400),
             note!(tap, 3500, 0.2),
-            note!(hold, 3500, 0.8, 400),
+            note!(hold, 3500, 0.8, 3900),
             note!(tap, 4000, 0.6),
-            note!(hold, 4000, 0.4, 500)
+            note!(hold, 4000, 0.4, 4500)
         ].into(),
         events: vec!().into()
     }
