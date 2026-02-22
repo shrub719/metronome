3a0b42d9667e1812c2a4a888046ce89d1d112a50
shrub <shrub719@gmail.com>
2026-02-22 15:17:26 +0000

    compile mtn_01 binary with new thingy

--- src/map/maps.rs
+++ src/map/maps.rs
@@ -1,15 +1,15 @@
-pub const N_MAPS: usize = 9;
+pub const N_MAPS: usize = 1;
 
 pub const MAPS: [&[u8]; N_MAPS] = [
     include_bytes!("../../assets/maps/mtb/mtn_01.mtb"),
-    include_bytes!("../../assets/maps/mtb/black_world.mtb"),
-    include_bytes!("../../assets/maps/mtb/new_magic_wand.mtb"),
-    include_bytes!("../../assets/maps/mtb/test.mtb"),
-    include_bytes!("../../assets/maps/mtb/competent.mtb"),
-    include_bytes!("../../assets/maps/mtb/see_you_again.mtb"),
-    include_bytes!("../../assets/maps/mtb/balloon.mtb"),
-    include_bytes!("../../assets/maps/mtb/here_comes_a_thought.mtb"),
-    include_bytes!("../../assets/maps/mtb/stronger_than_you.mtb")
+    // include_bytes!("../../assets/maps/mtb/black_world.mtb"),
+    // include_bytes!("../../assets/maps/mtb/new_magic_wand.mtb"),
+    // include_bytes!("../../assets/maps/mtb/test.mtb"),
+    // include_bytes!("../../assets/maps/mtb/competent.mtb"),
+    // include_bytes!("../../assets/maps/mtb/see_you_again.mtb"),
+    // include_bytes!("../../assets/maps/mtb/balloon.mtb"),
+    // include_bytes!("../../assets/maps/mtb/here_comes_a_thought.mtb"),
+    // include_bytes!("../../assets/maps/mtb/stronger_than_you.mtb")
 ];
 
 // UPDATE N_MAPS!!
