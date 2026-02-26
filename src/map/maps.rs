544b7b2220d9bdb0e3b53b5a7c450b309d63698d
shrub <shrub719@gmail.com>
2026-02-26 14:42:13 +0000

    include spoken_for

--- src/map/maps.rs
+++ src/map/maps.rs
@@ -5,6 +5,7 @@ const METRONOME: MapPack = MapPack {
     maps: &[
         include_bytes!("../../assets/maps/metronome/mtn_01.mtb"),
         include_bytes!("../../assets/maps/metronome/mtn_02.mtb"),
+        include_bytes!("../../assets/maps/metronome/mtn_03.mtb"),
         include_bytes!("../../assets/maps/metronome/test.mtb"),
     ],
     color: Color565::from_rgb(255, 183, 52)
@@ -13,10 +14,9 @@ const METRONOME: MapPack = MapPack {
 const OSU: MapPack = MapPack {
     maps: &[
         include_bytes!("../../assets/maps/osu/black_world.mtb"),
+        include_bytes!("../../assets/maps/osu/spoken_for.mtb"),
         include_bytes!("../../assets/maps/osu/new_magic_wand.mtb"),
         include_bytes!("../../assets/maps/osu/see_you_again.mtb"),
-        include_bytes!("../../assets/maps/osu/balloon.mtb"),
-        include_bytes!("../../assets/maps/osu/competent.mtb"),
         include_bytes!("../../assets/maps/osu/here_comes_a_thought.mtb"),
         include_bytes!("../../assets/maps/osu/stronger_than_you.mtb"),
     ],
